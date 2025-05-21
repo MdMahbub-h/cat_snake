4289ffba3d439cca61a6453fcc73145e3156f3ea
MdMahbub-h <2021339001@student.sust.edu>
2025-05-21 08:23:43 +0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ const config = {
         default: "arcade",
         arcade: {
             gravity: { y: 0 },
-            debug: true,
+            debug: false,
         },
     },
     scene: [
